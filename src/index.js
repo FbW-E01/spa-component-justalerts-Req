70430065ae02d68a21dd9531602c598f68f33608
Joel Peltonen <joel.peltonen@digitalcareerinstitute.org>
2021-07-12 09:40:39 +0200

    Add Solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Box = (/* ???? */) => {
+class Box extends React.Component {
 
-  // ?????
-  
+  getType = () => {
+    switch (this.props.type) {
+      case "red":
+        return "danger";
+      case "orange":
+        return "warning";
+      default:
+        return "primary";
+    }
+  }
+
+  render() {
+    console.log(this.props);
+
+    if (this.props.hide === true) {
+      return null;
+    }
+
+    const type = this.getType();
+    
+    return (
+      <div className={`alert alert-${type}`} role="alert">
+        {this.props.message}
+      </div>
+    );
+  }
 };
 
 // Do not edit below this line
-const jsx = <>
+const myApplication = <React.Fragment>
   <Box type="blue" message="Blue box" />
   <Box type="red" message="Red box" />
   <Box type="orange" message="Orange box" />
   <Box hide type="red" message="Hidden box" />
-</>;
+</React.Fragment>;
 const element = document.getElementById("target");
 
-ReactDOM.render(jsx, element);
+ReactDOM.render(myApplication, element);
